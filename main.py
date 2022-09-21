7ab60addaac21a1aa3f5b0a85ae914f405020a9c
alex1010101010 <94766326+alex1010101010@users.noreply.github.com>
2022-09-21 16:30:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import smtplib
 import yfinance as yf
-
-gmail_user = 'clickforthedeal@gmail.com'
-gmail_password = 'hqbvtqpgvsdfuztc'
+import creds
 
 
 eur_usd = round(yf.Ticker('EURUSD=X').history(period='1d')['Close'][0],2)
 gbp_usd = round(yf.Ticker('GBPUSD=X').history(period='1d')['Close'][0],2)
 eur_gbp = round(yf.Ticker('EURGBP=X').history(period='1d')['Close'][0],2)
 
-sent_from = gmail_user
-to = ['alexspellanzon@gmail.com']
+sent_from = creds.gmail_user
+to = [creds.receiver]
 subject = 'Daily FX rates'
 body = f'EUR to USD {eur_usd}, GBP to USD {gbp_usd}, EUR to GBP {eur_gbp}'
 
@@ -25,7 +23,7 @@ Subject: %s
 try:
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.ehlo()
-    smtp_server.login(gmail_user, gmail_password)
+    smtp_server.login(creds.gmail_user, creds.gmail_password)
     smtp_server.sendmail(sent_from, to, email_text)
     smtp_server.close()
     print ("Email sent successfully!")
