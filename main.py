4e98f32959fb4d97a274add46647eef0a5ddd77b
Alex Spellanzon <alexspellanzon@gmail.com>
2022-09-21 21:59:06 +0100

    hiding test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ eur_usd = round(yf.Ticker('EURUSD=X').history(period='1d')['Close'][0],2)
 gbp_usd = round(yf.Ticker('GBPUSD=X').history(period='1d')['Close'][0],2)
 eur_gbp = round(yf.Ticker('EURGBP=X').history(period='1d')['Close'][0],2)
 
-sent_from = creds.gmail_user
+sent_from = creds.sender
 to = [creds.receiver]
 subject = 'Daily FX rates'
 body = f'EUR to USD {eur_usd}, GBP to USD {gbp_usd}, EUR to GBP {eur_gbp}'
@@ -23,7 +23,7 @@ Subject: %s
 try:
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.ehlo()
-    smtp_server.login(creds.gmail_user, creds.gmail_password)
+    smtp_server.login(creds.sender, creds.password)
     smtp_server.sendmail(sent_from, to, email_text)
     smtp_server.close()
     print ("Email sent successfully!")
