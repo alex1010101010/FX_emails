ce01fd4f1cbe66c3a57de8de67ae093573c4e27e
Alex Spellanzon <alexspellanzon@gmail.com>
2022-09-21 22:39:46 +0100

    hiding test

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import smtplib
 import yfinance as yf
-import creds
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
 
 
 eur_usd = round(yf.Ticker('EURUSD=X').history(period='1d')['Close'][0],2)
 gbp_usd = round(yf.Ticker('GBPUSD=X').history(period='1d')['Close'][0],2)
 eur_gbp = round(yf.Ticker('EURGBP=X').history(period='1d')['Close'][0],2)
 
-sent_from = creds.sender
-to = [creds.receiver]
+sent_from = os.getenv('sender')
+to = [os.getenv("receiver")]
 subject = 'Daily FX rates'
 body = f'EUR to USD {eur_usd}, GBP to USD {gbp_usd}, EUR to GBP {eur_gbp}'
 
@@ -23,7 +28,7 @@ Subject: %s
 try:
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.ehlo()
-    smtp_server.login(creds.sender, creds.password)
+    smtp_server.login(os.getenv("sender"), os.getenv("password"))
     smtp_server.sendmail(sent_from, to, email_text)
     smtp_server.close()
     print ("Email sent successfully!")
